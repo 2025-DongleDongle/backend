99db7de88cf3ab33b12b2b960eaff4144bbb1adc
gyuuuuri <gyuri113@ewha.ac.kr>
2025-11-26 11:28:26 +0900

    ♻️ refactor: 세부지출항목 수정

--- budgets/urls.py
+++ budgets/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('fill/', BudgetView.as_view(), name='budget-view'),
     path('base-average/', BaseAverageView.as_view(), name='base-average-view'),
     path('living-average/',LivingAvgView.as_view(), name='living-average-view'),
-    path('total-average/', TotalAvgView.as_view(), name='total-average-view')
+    path('total-average/', TotalAvgView.as_view(), name='total-average-view'),
 ]
