30d7339d6ba8deb845228df928c9ec0e3ed02a80
gyuuuuri <gyuri113@ewha.ac.kr>
2025-11-27 23:16:40 +0900

    ♻️ refactor: 예산안 최대 자릿수 수정(1)

--- dongleDongle/settings/base.py
+++ dongleDongle/settings/base.py
@@ -49,7 +49,6 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'dj_rest_auth.registration',
-    'django_extensions',
 
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
